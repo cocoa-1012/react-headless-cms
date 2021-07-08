b45e0023f282673049aab11caa55d1e6d2111e06
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-07-08 11:33:28 -0400

    net-commit=test

--- src/components/Exhibitions/exhibition.js
+++ src/components/Exhibitions/exhibition.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
-
 
 export const ContainerDiv = styled.div`
   display: block;
@@ -31,65 +30,72 @@ export const DetailText = styled.div`
   max-width: 960px;
 `;
 
-const data = [{
+const data = [
+  {
     imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
     author: "John Key",
     title: "Lean on me",
     duration: "May 8-June 19. 2021",
-}, {
+  },
+  {
     imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
     author: "Kiyomi Quinn Taylor",
     title: "A River Gets Wide",
     duration: "May 8-June 19. 2021",
-}, {
+  },
+  {
     imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
     author: "Drew Dodge",
     title: "Wasteland",
     duration: "May 8-June 19. 2021",
-},
-]
+  },
+];
 
 const Artist = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    return (
-        <ContainerDiv>
-            <TitleText>{id} exhibitions</TitleText>
-            {data.map((item, key) => (
-                <div
-                    key={key}
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        maxWidth: 960,
-                        marginBottom: 20
-                    }}
-                >
-                    <div style={{ marginRight: 350 }}>
-                        <DetailText>
-                            <a href="#" style={{ textDecoration: 'none' }}>{item.author}</a>
-                        </DetailText>
-                        <DetailText className="subtitle_head">{item.title}</DetailText>
-                        <p></p>
-                        <DetailText>{item.duration}</DetailText>
-                        <a href="#" style={{ textDecoration: "none" }}><DetailText style={{ color: "#d12a2f" }}>Read more&nbsp;&nbsp; &rarr; </DetailText></a>
-
-                    </div>
-                    <a href="#">
-                        <img
-                            width="450"
-                            height="200"
-                            src={item.imgUrl}
-                            alt=""
-                            sizes="(max-width: 1000px) 100vw, 1000px"
-                        />
-                    </a>
-
-                </div>
-            ))}
-        </ContainerDiv>
-    );
-}
+  return (
+    <ContainerDiv>
+      <TitleText>{id} exhibition</TitleText>
+      {data.map((item, key) => (
+        <div
+          key={key}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            maxWidth: 960,
+            marginBottom: 20,
+          }}
+        >
+          <div style={{ marginRight: 350 }}>
+            <DetailText>
+              <a href="#" style={{ textDecoration: "none" }}>
+                {item.author}
+              </a>
+            </DetailText>
+            <DetailText className="subtitle_head">{item.title}</DetailText>
+            <p></p>
+            <DetailText>{item.duration}</DetailText>
+            <a href="#" style={{ textDecoration: "none" }}>
+              <DetailText style={{ color: "#d12a2f" }}>
+                Read more&nbsp;&nbsp; &rarr;{" "}
+              </DetailText>
+            </a>
+          </div>
+          <a href="#">
+            <img
+              width="450"
+              height="200"
+              src={item.imgUrl}
+              alt=""
+              sizes="(max-width: 1000px) 100vw, 1000px"
+            />
+          </a>
+        </div>
+      ))}
+    </ContainerDiv>
+  );
+};
 
 export default Artist;
