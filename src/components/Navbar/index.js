ff08f1b28e2dce367b86091ddf2919aa05331793
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-07-20 17:59:23 -0400

    000

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,20 @@
 import React, { useRef, useState } from "react";
 import FocusLock from "react-focus-lock";
-// import Burger from "../Burger/index";
-// import Menu from "../Menu/index";
+import logoImg from "../../assets/images/logo/logo.png";
+import Burger from "../Burger/index";
+import Menu from "../Menu/index";
 import {
-  // DropDownContainer,
-  // DropDownList,
-  // DropDownListContainer,
+  DropDownContainer,
+  DropDownList,
+  DropDownListContainer,
   GeneralDiv,
   ImgLogo,
-  // ListItem,
+  ListItem,
   Nav,
   NavContainer,
   NavLink,
   NavMenu,
+  SubLink,
 } from "./NavbarElements";
 
 const options1 = ["Lita Albuquerque"];
@@ -55,13 +57,13 @@ const Navbar = () => {
         <Nav>
           <NavLink to="/">
             <ImgLogo>
-              {/* <img src={logoImg} alt="Bond Street" /> */}
-              <p>ttt ttt</p>
+              <img src={logoImg} alt="Bond Street" />
+              {/* <p>ttt ttt</p> */}
             </ImgLogo>
           </NavLink>
 
           <NavMenu>
-            {/* <DropDownContainer
+            <DropDownContainer
               onMouseOver={handleMouseOver1}
               onTouchStart={handleMouseOver1}
               onMouseLeave={handleMouseLeave1}
@@ -113,13 +115,13 @@ const Navbar = () => {
               )}
             </DropDownContainer>
             <NavLink to="/gallaryVault">GallaryVault</NavLink>
-            <NavLink to="/info">Info</NavLink> */}
+            <NavLink to="/info">Info</NavLink>
           </NavMenu>
 
           <GeneralDiv ref={node}>
             <FocusLock disabled={!open}>
-              {/* <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-              <Menu open={open} setOpen={setOpen} id={menuId} /> */}
+              <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+              <Menu open={open} setOpen={setOpen} id={menuId} />
             </FocusLock>
           </GeneralDiv>
         </Nav>
