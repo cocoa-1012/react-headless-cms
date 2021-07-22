bc6ef5ac64027a4be098b44af562fbb0fc6478ba
cocoa-1012 <cocoa.pandaok@gmail.com>
2021-07-22 02:27:05 -0400

    really

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { ContainerHomeDiv, DetailInfoDiv, HomeImg } from "./subPageStyle";
+// import { ContainerHomeDiv, DetailInfoDiv, HomeImg } from "./subPageStyle";
 
-const data = [
-  {
-    imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
-    author: "John Key",
-    title: "Lean on me",
-    duration: "May 8-June 19. 2021",
-  },
-  {
-    imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
-    author: "Kiyomi Quinn Taylor",
-    title: "A River Gets Wide",
-    duration: "May 8-June 19. 2021",
-  },
-  {
-    imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
-    author: "Drew Dodge",
-    title: "Wasteland",
-    duration: "May 8-June 19. 2021",
-  },
-];
+// const data = [
+//   {
+//     imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
+//     author: "John Key",
+//     title: "Lean on me",
+//     duration: "May 8-June 19. 2021",
+//   },
+//   {
+//     imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
+//     author: "Kiyomi Quinn Taylor",
+//     title: "A River Gets Wide",
+//     duration: "May 8-June 19. 2021",
+//   },
+//   {
+//     imgUrl: "../../assets/images/home/home-1 - Lean-On-Me_Install-22.jpg",
+//     author: "Drew Dodge",
+//     title: "Wasteland",
+//     duration: "May 8-June 19. 2021",
+//   },
+// ];
 // const addressInfo = "6830 Santa Monica Blvd. Los Angeles CA 90038";
 // const idInfo = "323.460.6830";
 // const mailInfo = "info@steveturner.la";
@@ -28,7 +28,8 @@ const data = [
 const Home = () => {
   return (
     <>
-      {data.map((item, key) => (
+      ABCDE...
+      {/* {data.map((item, key) => (
         <ContainerHomeDiv key={key}>
           <HomeImg
             src={item.imgUrl}
@@ -46,7 +47,7 @@ const Home = () => {
             <p>{item.duration}</p>
           </DetailInfoDiv>
         </ContainerHomeDiv>
-      ))}
+      ))} */}
     </>
   );
 };
